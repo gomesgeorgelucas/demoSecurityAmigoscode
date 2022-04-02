e088110625ea79fa2113e1380c51c42dd747dfd7
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-04-02 17:17:36 -0400

    create in memory user and passwd from userDetailsService - role STUDENT - Missing PasswordEncoder

--- src/main/java/com/example/demosecurityamigoscode/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demosecurityamigoscode/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.demosecurityamigoscode.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Enable and configure everything that has to do with security in the application.
@@ -21,5 +26,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic(); //there is no way to logout as username and password are validated in every request
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password("password")
+                .roles("STUDENT") //ROLE_STUDENT -> ROLE_ prefix automatically added since spring security 4.
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser
+        );
     }
 }
