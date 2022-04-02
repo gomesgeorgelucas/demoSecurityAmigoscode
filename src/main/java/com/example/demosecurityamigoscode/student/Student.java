229eef61065ce91245aac9db797192c65bfaa077
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-04-02 18:32:10 -0400

    addet toString to student created api with management endpoints for testing - .csrf() disabled for testing

--- src/main/java/com/example/demosecurityamigoscode/student/Student.java
+++ src/main/java/com/example/demosecurityamigoscode/student/Student.java
@@ -3,9 +3,11 @@ package com.example.demosecurityamigoscode.student;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @Builder
+@ToString
 @AllArgsConstructor
 public class Student {
     final Integer studentId;
