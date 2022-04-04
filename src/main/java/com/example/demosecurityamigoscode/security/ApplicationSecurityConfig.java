ec24284b15173ec04e859a683b871e8be4704827
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-04-04 02:33:34 -0400

    authority based security using enums created - using SimpleGrantedAuthority

--- src/main/java/com/example/demosecurityamigoscode/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demosecurityamigoscode/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.demosecurityamigoscode.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.demosecurityamigoscode.security.ApplicationUserRole.*;
 
 /**
@@ -33,6 +35,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+                .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -45,19 +51,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name()) //ROLE_STUDENT -> ROLE_ prefix automatically added since spring security 4.
+//                .roles(STUDENT.name()) //ROLE_STUDENT -> ROLE_ prefix automatically added since spring security 4.
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name()) //ROLE_ADMIN
+//                .roles(ADMIN.name()) //ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN_TRAINEE.name()) //ROLE_ADMIN_TRAINEE
+//                .roles(ADMIN_TRAINEE.name()) //ROLE_ADMIN_TRAINEE
+                .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
--- src/main/java/com/example/demosecurityamigoscode/security/ApplicationUserRole.java
+++ src/main/java/com/example/demosecurityamigoscode/security/ApplicationUserRole.java
@@ -3,8 +3,10 @@ package com.example.demosecurityamigoscode.security;
 import com.google.common.collect.Sets;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.demosecurityamigoscode.security.ApplicationUserPermission.*;
 
@@ -16,4 +18,13 @@ public enum ApplicationUserRole {
     ADMIN_TRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
 
     final Set<ApplicationUserPermission> permissions;
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_"+ this.name()));
+        return permissions;
+    }
 }
